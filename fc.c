4c65fc472eaecdee647372676eb40778e8b9bd3e
Adam Nasi <adamrnasi@gmail.com>
2020-09-29 14:16:25 -0400

    1.3

--- fc.c
+++ fc.c
@@ -3,40 +3,17 @@
 void celsiusToFahrenheit();
 
 int main() {
-	float fahr, celsius;
-	float lower, upper, step;
-
-	lower = 0;
-	upper = 300;
-	step = 20;
-
-	fahr = lower;
-
 	printf("====== F to C ======\n");
-	while (fahr <= upper) {
-		celsius = (5.0 / 9.0) * (fahr - 32);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+	int fahr;
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+		printf("%3d %6.1f\n", fahr, ( 5.0 / 9.0 ) * ( fahr - 32 ));
 	}
-
-	celsiusToFahrenheit();
-	return 0;
-}
-
-void celsiusToFahrenheit() {
-	float fahr, celsius;
-	float lower, upper, step;
-
-	lower = -20;
-	upper = 100;
-	step = 10;
-
-	celsius = lower;
 
 	printf("====== C to F ======\n");
-	while (celsius <= upper) {
-		fahr = (9.0 / 5.0) * (celsius + 32);
-		printf("%3.0f %6.1f\n", celsius, fahr);
-		celsius = celsius + step;
+	int celsius;
+	for (celsius = -20; celsius <= 100; celsius = celsius + 10) {
+		printf("%3d %6.1f\n", celsius, ( 9.0 / 5.0 ) * celsius + 32);
 	}
+
+	return 0;
 }
