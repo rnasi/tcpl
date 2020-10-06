42ef0364a047422ed3d05c2823e6be5b7c987177
Adam Nasi <nasiar@dgs1sdt.com>
2020-10-06 08:49:11 -0400

    line wrapper breaks words

--- sec10/ex1-22.c
+++ sec10/ex1-22.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-#define SCREEN_WIDTH	10
+#define SCREEN_WIDTH	80
 #define MAX_WIDTH	1000
 
 int getLine(char line[], int limit);
-void wrapLine(char line[], int length, int screenWidth);
+int wrapLine(char line[], int length, int screenWidth, int maxLength);
 
 int main() {
 	char line[MAX_WIDTH];
 	int length;
 
 	while ((length = getLine(line, MAX_WIDTH)) > 0) {
-		wrapLine(line, length, SCREEN_WIDTH);
+		int statusCode;
+		if ((statusCode = wrapLine(line, length, SCREEN_WIDTH, MAX_WIDTH)) != 0)
+			return statusCode;
 		printf("%s", line);
 	}
 	
 	return 0;
 }
 
-// getLine: Copies input into 'line' until 'limit' or a newline is reached and returns length
+// getLine: Copies input into 'line' until 'limit' or a newline is reached
+// ret: length of line
 int getLine(char line[], int limit) {
 	int c;
 	int length = 0;
@@ -37,19 +40,41 @@ int getLine(char line[], int limit) {
 	return length;
 }
 
-// wrapLine: inserts newlines at first blank before 'screenWidth'. wraps unbroken lines at 'screenWidth'
-void wrapLine(char line[], int length, int screenWidth) {
+// wrapLine: inserts newlines at first blank before 'screenWidth'. Wraps unbroken lines at 
+// 	'screenWidth'
+// ret: status code	0 - OK
+// 			1 - Max length exceeded. Since a character is added to the string when a
+// 				line has no whitespace, we need to make sure the wrapped string 
+// 				does not exceed the max width.
+int wrapLine(char line[], int length, int screenWidth, int maxLength) {
 	int i = screenWidth;
 
 	while (i < length) {
 		int j;
-		for (j = 1; line[i - j] != ' ' && j < screenWidth; j++);
-		if (j == screenWidth)
-			;// something
-		else
-			line[i - j] = '\n';
+		// Find last space in the line before the screen width
+		for (j = 0; line[i - j] != ' ' && j < screenWidth; j++);
 
-//		printf("%d  %d\n", i, j);
-		i = i - j + screenWidth;
+		// There were no spaces before screen width; need to insert one
+		if (j == screenWidth) {
+			if (length < maxLength - 1) {
+				// Increment length, shift array over, insert newline
+				length++;
+				int k;
+				for (k = length; k > i; k--)
+					line[k] = line[k - 1];
+
+				line[i] = '\n';
+				line[length + 1] = '\0';
+				i += screenWidth + 1;
+			} else
+				return 1; // Max length exceeded
+
+		// There was a space; make it a newline
+		} else {
+			line[i - j] = '\n';
+			i = i - j + screenWidth;
+		}
 	}
+
+	return 0;
 }
