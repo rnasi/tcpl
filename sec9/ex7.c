69585baa53356e53d2bcd82416683ae60e795485
Adam Nasi <nasiar@dgs1sdt.com>
2020-10-07 15:47:55 -0400

    2.9

---
+++ sec9/ex7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int invert(const int, const int, const int);
+
+// Write a function `invert(x,p,n)` that returns `x` with the `n` bits that begin at position `p`
+// inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
+int main() {
+	// 178 = 10 1100 10, flip 4 at position 5 -> 10 0011 1	0 = 142
+	printf("%d\n", invert(178, 5, 4));
+
+	return 0;
+}
+
+// invert:	inverts `n` bits of `x` starting at `p`
+// assumptions:	`n` and `p` are sensible positive integers
+// return:	the result of the inversion
+int invert(const int x, const int p, const int n) {
+	return (x & (~0 << (p + 1)))			// left part
+		+ (~x & (~(~0 << n) << (p - n + 1)))	// inverted part
+		+ (x & ~(~0 << (p - n + 1)));		// right part
+}
---
+++ sec9/ex8.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define X	178	// 10110010
+#define N	3	// will yield 010 ... 10110 when right rotated
+			// 32-bit: 01000000000000000000000000010110 = 1073741846
+
+int rightrot(const int, const int);
+int intsize();
+
+// Write a function `rightrot(x,n)` that returns the value of the integer `x` rotated to the right
+// by `n` positions.
+int main() {
+	printf("Int size in bits on current machine: %d\n"
+			"%d rotated right %d positions: %d\n"
+			, intsize(), X, N, rightrot(X, N));
+}
+
+// intsize:	determines the size of an `int`
+// return:	size of `int` in bits
+int intsize() {
+	int i, x;
+	x = 1;
+	// Shift `x` left until overflow; `i` will be int size - 1 (not counting rightmost bit).
+	for (i = 1; (x = x << 1) > 0; i++);
+	return i + 1;
+}
+
+// rightrot:	rotates `x` to the right by `n` positions
+// assumptions:	`n` is a sensible positive integet
+// return:	rotated value
+int rightrot(const int x, const int n) {
+	return (x >> n)				// left part
+		+ (x << (intsize() - n));	// right part
+}
+
