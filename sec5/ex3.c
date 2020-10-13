65e320fa33307462b65eab04f3ae62dc0ab6d4a0
Adam Nasi <nasiar@dgs1sdt.com>
2020-10-13 10:32:24 -0400

    ex 3-3 done with upper case ranges and genericized range expanding function

--- sec5/ex3.c
+++ sec5/ex3.c
@@ -28,11 +28,10 @@ int main() {
 	char str12[] = "This has 9-9 which is just a \'9\'";
 	char str13[] = "This has 2-5-6 which is weird";
 
-	char str14[] = "This one has a lot of different things like a-z, 0-9, maybe some e-y and 3-3-6-7-7.";
+	char str14[] = "This one has a lot of different things like a-z, A-Z, 0-9, maybe some e-y and 3-3-6-7-7.";
 
 	
 	printExpanded(str1);
-	printExpanded(str2);
 	printExpanded(str2);
 	printExpanded(str3);
 	printExpanded(str4);
@@ -52,10 +51,24 @@ int main() {
 	return 0;
 }
 
-void copyRange(char start, char end, const char input[], char output[], int iIn, int iOut) {
+//copyRange:	expands a shorhanded char range defined as `start`-`end` if present at iIn
+//assumptions:	`output` is big enough
+//return:	new value for `iOut` if char shorthand is found at `input[iIn]`; 0 otherwise
+int copyRange(char start, char end, const char input[], char output[], int iIn, int iOut) {
+	int j;
+	if (input[iIn] >= start  && input[iIn] <= end
+			&& input[iIn + 1] == '-'
+			&& input[iIn + 2] >= input[iIn] && input[iIn + 2] <= end) {
 
+		// Start at first letter, go to last letter (not inclusive)
+		for (j = input[iIn] - start; j < input[iIn + 2] - start; iOut++, j++)
+			output[iOut] = start + j;
 
+		// Above loop increments iOut one too many times
+		return iOut - 1;
+	}
 
+	return 0;
 }
 
 //expand:	expands shorthand notations (e.g. a-z) in `str1` into `str2`
@@ -65,35 +78,21 @@ void expand(const char input[], char output[]) {
 	int inLen = strlen(input);
 
 	for (iIn = 0, iOut = 0; iIn < inLen - 2; iIn++, iOut++) {
-
-
-
-		// Letter range found
-		if (input[iIn] >= 'a'  && input[iIn] <= 'z'
-				&& input[iIn + 1] == '-'
-				&& input[iIn + 2] >= input[iIn] && input[iIn + 2] <= 'z') {
-
-			// Start at first letter, go to last letter (not inclusive)
-			for (j = input[iIn] - 'a'; j < input[iIn + 2] - 'a'; iOut++, j++)
-				output[iOut] = 'a' + j;
-			// Skip '-' in input
-			iIn ++;
-			// Output index was incremented one too many times as a result of the previous loop
-			iOut--;
-
-		// Number range found
-		} else if (input[iIn] >= '0'  && input[iIn] <= '9'
-				&& input[iIn + 1] == '-'
-				&& input[iIn + 2] >= input[iIn] && input[iIn + 2] <= '9') {
-			
-			// Start at first letter, go to last letter (not inclusive)
-			for (j = input[iIn] - '0'; j < input[iIn + 2] - '0'; iOut++, j++)
-				output[iOut] = '0' + j;
-			// Skip '-' in input
-			iIn ++;
-			// Output index was incremented one too many times as a result of the previous loop
-			iOut--;
-
+		int shorthand;
+		// Lower case letter range shorthand found
+		if ((shorthand = copyRange('a', 'z', input, output, iIn, iOut))) {
+			// Shorthand found--advance iOut
+			iOut = shorthand;
+			// skip '-' in input
+			iIn++;
+		// Upper case letter range
+		} else if ((shorthand = copyRange('A', 'Z', input, output, iIn, iOut))) {
+			iOut = shorthand;
+			iIn++;
+		// Digit range
+		} else if ((shorthand = copyRange('0', '9', input, output, iIn, iOut))) {
+			iOut = shorthand;
+			iIn++;
 		// No shorthand, just copy character
 		} else
 			output[iOut] = input[iIn];
